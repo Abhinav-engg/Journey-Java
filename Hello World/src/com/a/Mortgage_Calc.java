2885f6319539e6979629bf2f6a7282365eaa8529
Abhinav-engg <abhinav-69@outlook.com>
2025-07-13 22:17:49 +0530

    More Readable

--- Hello World/src/com/a/Mortgage_Calc.java
+++ Hello World/src/com/a/Mortgage_Calc.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Mortgage_Calc {
     public static void main(String[] args) throws Exception {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the principal amount:");
         int principal1 = sc.nextInt();
@@ -15,14 +18,13 @@ public class Mortgage_Calc {
         int time = sc.nextInt();
         sc.close();
 
-        float monthlyInterest = annualRate / 12 / 100;
-        int numberOfPayments = time * 12;
+        float monthlyInterest = annualRate / MONTHS_IN_YEAR / PERCENT;
+        int numberOfPayments = time * MONTHS_IN_YEAR;
 
         float monthlyPayment = principal1 * (monthlyInterest * (float)Math.pow(1 + monthlyInterest, numberOfPayments)) / ((float)Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         System.out.println("Your monthly payment is:" + monthlyPayment);
     }
 }
-
 
 
